5e6f3a49a5ba07bc9437203c0206536d2c4d6476
y368413 <45956954@qq.com>
2019-09-01 20:46:01 +0800

    0.1.7

--- Interface/AddOns/_ShiGuang/Plugins/yClassColors.lua
+++ Interface/AddOns/_ShiGuang/Plugins/yClassColors.lua
@@ -96,6 +96,48 @@ hooksecurefunc("GuildStatus_Update", function()
 	end
 end)
 
+-- Friends
+local FRIENDS_LEVEL_TEMPLATE = FRIENDS_LEVEL_TEMPLATE:gsub("%%d", "%%s")
+FRIENDS_LEVEL_TEMPLATE = FRIENDS_LEVEL_TEMPLATE:gsub("%$d", "%$s")
+
+local function friendsFrame()
+	local scrollFrame = FriendsFrameFriendsScrollFrame
+	local buttons = scrollFrame.buttons
+	local playerArea = GetRealZoneText()
+
+	for i = 1, #buttons do
+		local nameText, infoText
+		local button = buttons[i]
+		if button:IsShown() then
+			if button.buttonType == FRIENDS_BUTTON_TYPE_WOW then
+				local info = C_FriendList.GetFriendInfoByIndex(button.id)
+				if info and info.connected then
+					nameText = classColor(info.className)..info.name.."|r, "..format(FRIENDS_LEVEL_TEMPLATE, diffColor(info.level)..info.level.."|r", info.className)
+					if info.area == playerArea then
+						infoText = format("|cff00ff00%s|r", info.area)
+					end
+				end
+			elseif button.buttonType == FRIENDS_BUTTON_TYPE_BNET then
+				local _, presenceName, _, _, _, gameID, client, isOnline = BNGetFriendInfo(button.id)
+				if isOnline and client == BNET_CLIENT_WOW then
+					local _, charName, _, _, _, faction, _, class, _, zoneName = BNGetGameAccountInfo(gameID)
+					if presenceName and charName and class and faction == UnitFactionGroup("player") then
+						nameText = presenceName.." "..FRIENDS_WOW_NAME_COLOR_CODE.."("..classColor(class)..charName..FRIENDS_WOW_NAME_COLOR_CODE..")"
+						if zoneName == playerArea then
+							infoText = format("|cff00ff00%s|r", zoneName)
+						end
+					end
+				end
+			end
+		end
+
+		if nameText then button.name:SetText(nameText) end
+		if infoText then button.info:SetText(infoText) end
+	end
+end
+hooksecurefunc(FriendsFrameFriendsScrollFrame, "update", friendsFrame)
+hooksecurefunc("FriendsFrame_UpdateFriends", friendsFrame)
+
 -- Whoframe
 local columnTable = {}
 local function updateWhoList()
